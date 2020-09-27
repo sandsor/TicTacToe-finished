fce1927cee62be0ef4b59a30ebd78878086e1772
sandsor <70569977+sandsor@users.noreply.github.com>
2020-09-27 17:31:47 +0200

    Finished game. about to style and perfect...

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -5,24 +5,38 @@
 char number[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 void board();
-void checkwin();
+int checkwin();
 int main()
 {
-	int player;
+	int player = 1;
 	int y;
 	char symbol = 'x';
-	int choice;
+	
+	/*int choice;
+	if (player == 0) {
+		player == 1;
+	}
 
+	else {
+		player == 2;
+	}*/
 	
 	do
 	{
 		board();
 
+		if (player==3) {
+			player--;
+			player--;
+		}
+		int choice;
 		
+		symbol = (player == 1) ? 'X' : 'O'; // if player == 1 then use 'X' if not then player = 'O'
 
-
-		std::cout << "please choose a slot :";
+		std::cout << "please choose a number player " << player << " :";
 		std::cin >> choice;
+		
+		
 
 		if (choice == 1 && number[1] == '1') {
 			number[1] = symbol;
@@ -53,25 +67,67 @@ int main()
 		}
 		else {
 			std::cout << "invalid input";
+			player--;
 		}
-		y = -1;
-		/*y = checkwin();*/
+		
+		
+		y = checkwin();
+		player++;
 
-		/*player++;*/
+
+
 	} while (y == -1);
 	board();
-
-	return 0;
+	player--;
+	if (y == 1) {
+		std::cout << "Player " << player << " wins";
+	}
+	else {
+		std::cout << "Game Draw";
+	}
+	
+	
+	return(0);
 }
 
-void checkwin() {
+// when a player types a number and it gets printed to the board, checkwin() gets run and sends a value back into the main function using Y. if we do return(1) in checkwin() then y gets a
+// value of 1. so using that i make it so that 1 is for when a player wins, -1 is for continue with the game and 0 is draw with no winner.
+
+
+int checkwin() {
 	
-	
-	
-	
-	/*return 0;
-	else
-	return -1;*/
+	if (number[1] == number[2] && number[2] == number[3]) {
+		return 1;
+	}
+	else if (number[4] == number[5] && number[5] == number[6]) {
+		return 1;
+	}
+	else if (number[7] == number[8] && number[8] == number[9]) {
+		return 1;
+	}
+	else if (number[1] == number[4] && number[4] == number[7]) {
+		return 1;
+	}
+	else if (number[2] == number[5] && number[5] == number[8]) {
+		return 1;
+	}
+	else if (number[3] == number[6] && number[6] == number[9]) {
+		return 1;
+	}
+	else if (number[7] == number[5] && number[5] == number[3]) {
+		return 1;
+	}
+	else if (number[9] == number[5] && number[5] == number[1]) {
+		return 1;
+	}
+	else if (number[1] != '1' && number[2] != '2' && number[3] != '3' && number[4] != '4' && number[5] != '5'
+		&& number[6] != '6' && number[7] != '7' && number[8] != '8' && number[9] != '9') 
+	{
+		return 0;
+	}
+	else {
+		return -1;
+	}
 }
 
 
